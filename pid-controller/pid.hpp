5cd04bada2bc58749374434486c3d8351d4652a0
Lauren Dudgeon <99220320+laurendudgeon@users.noreply.github.com>
2023-10-26 11:39:17 -0400

    Update pid.hpp
    Adding variables for pid controller

--- pid-controller/pid.hpp
+++ pid-controller/pid.hpp
@@ -1,1 +1,23 @@
 
+typedef struct {
+//Controller gains
+float kp;
+float ki;
+float kd;
+
+//output limit
+float minLimit;
+float maxLimit;
+
+//sample time (seconds)
+float T;
+
+//memory needed for integrator and differentiator
+float integrator;
+float differentiator;
+float prevError;
+float prevMeasure;
+
+//output of controller
+float output;
+}

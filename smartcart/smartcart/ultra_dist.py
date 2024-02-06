14393c0fb5bae19472f5aae5e8f7221cf47f27f1
wafeeqaj <72951837+wafeeqaj@users.noreply.github.com>
2024-02-06 11:14:03 -0500

    Updated ultra_dist

--- smartcart/smartcart/ultra_dist.py
+++ smartcart/smartcart/ultra_dist.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 import RPi.GPIO as GPIO
 import time
-from example_interfaces.msg import Int64
+from std_msgs.msg import Int32
 
 TRIG=21
 ECHO=20
@@ -11,11 +11,20 @@ distance=0
 
 class UltraDistanceNode(Node): 
 
-    def getDistance():
+    
+    def __init__(self):
+        super().__init__("distance_publisher") 
+        
+        self.distance_=0
+        self.distance_publisher_ = self.create_publisher(Int32, "distance", 10)
+        self.timer_ = self.create_timer(0.5, self.publish_distance)
+       
+
+    def getDistance(self):
         GPIO.setup(TRIG, GPIO.OUT)
         GPIO.setup(ECHO, GPIO.IN)
         GPIO.output(TRIG, False)
-        print("waiting for sensor to settle")
+        self.get_logger().info("waiting for sensor to settle")
         time.sleep(0.2)
         GPIO.output(TRIG, True)
         time.sleep(0.00001)
@@ -25,24 +34,20 @@ class UltraDistanceNode(Node):
         while GPIO.input(ECHO)==1:
             pulse_end=time.time()
         pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration*17150
+        distance= pulse_duration*17150
         distance=round(distance,2)
-        print("distance: ", distance, " cm")
-        time.sleep(2)
+        #self.get_logger().info("distance: ", self.distance_, " cm")
+        time.sleep(0.1)
         
+        return distance
 
-    def __init__(self):
-        super().__init__("distance_publisher") 
-        
-        self.distance_ =  distance
-        self.distance_publisher_ = self.create_publisher(Int64, "distance", 10)
-        self.timer_ = self.create_timer(1.0, self.publish_distance)
-        self.get_logger().info("Ultra Distance has been started")
 
     def publish_distance(self):
-        msg = Int64()
-        msg.data = self.distance_
+        
+        msg = Int32()
+        msg.data = int(self.getDistance())
         self.distance_publisher_.publish(msg)
+        self.get_logger().info("Ultra Distance has been started: %d" %msg.data)
 
 def main(args=None):
     rclpy.init(args=args)

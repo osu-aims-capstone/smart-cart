919176cadcce8373c470114d6352d193f64bd218
Lauren Dudgeon <99220320+laurendudgeon@users.noreply.github.com>
2024-02-15 12:26:00 -0500

    Update distance_calculation.py
    Node now publishes and subscribes correctly

--- smartcart/smartcart/distance_calculation.py
+++ smartcart/smartcart/distance_calculation.py
@@ -1,42 +1,40 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Int64
+from std_msgs.msg import Int32
 
 
 class DistanceCalculationNode(Node):
-
+    print('distance calculation started')
+    distance = -1
+    
     def __init__(self):
         super().__init__('distance_calculation_node')
         self.subscription = self.create_subscription(
-            Int64, 
+            Int32, 
             'distance', 
             self.distance_callback, 
             10)
-        distance = self.subscription    
-
-    def distance_callback(self, distance):
-        self.get_logger().info(distance)
+        self.publisher_ = self.create_publisher(Int32, "motorcommand", 10)
+        self.timer = self.create_timer(0.5, self.publish_motorcommand)
         
-    def __init__(self):
-        super().__init__('motor_command_publisher')
-        self.publisher_ = self.create_publisher(Int64, 'motorcommand', 10)
-        timer_period = 0.5 #secs
-        self.timer = self.create_timer(timer_period, self.publish_motorcommand)
-        self.get_logger().info("Motor Command Publisher has been started")
+    def distance_callback(self, msg):
+        self.get_logger().info("Distance received: %d" % msg.data)
+        self.distance = msg.data
 
-    def publish_motorcommand(self, distance):
+    def publish_motorcommand(self):
         max = 400 #cm
         min = 0.3 #cm
-
+        stopMotor = Int32()
+        
         #distance needed to stop motor
-        if (distance <= max and distance >= min):
-            stopMotor = 1
+        if (sel.distance <= max and self.distance >= min):
+            stopMotor.data = 1
         else:
-            stopMotor = 0
+            stopMotor.data = 0
 
         self.publisher_.publish(stopMotor)
-        self.get_logger().info("publishing: " % stopMotor)
+        self.get_logger().info("publishing: " % int(stopMotor.data))
 
 def main(args=None):
     rclpy.init(args=args)

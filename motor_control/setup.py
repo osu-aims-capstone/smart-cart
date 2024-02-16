84601e422ad8778f633ee039acbf90b9131e72c4
wafeeqaj <jaleel.5@osu.edu>
2024-02-16 15:40:27 -0500

    created Motor control package

---
+++ motor_control/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'motor_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wafeeqaj',
+    maintainer_email='jaleel.5@osu.edu',
+    description='Motor-control subscriber node',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['leftMotor = motor_control.leftMotorPublisher:main',
+                            'movement = motor_control.movementSubscriber:main',
+                            'rightMotor = motor_control.rightMotorPublisher:main',
+        ],
+    },
+)
